4e479714ee6be05100bc74cacd764c80f321f0fe
Akshay Elavia <akshay.elavia@sjsu.edu>
2020-03-07 15:53:22 -0800

    basic goroutines to download file parts

--- file_downloader.go
+++ file_downloader.go
@@ -5,16 +5,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"sync"
 )
 
 func main() {
+
+	var wg sync.WaitGroup
 
 	if len(os.Args) > 3 || len(os.Args) < 2 {
 		log.Fatalf("Usage: ./file_downloader URL num_threads")
 	}
 
 	url := os.Args[1]
-	numThreads := os.Args[2]
+	numThreads, _ := strconv.Atoi(os.Args[2])
 
 	fmt.Println(url)
 	fmt.Println(numThreads)
@@ -23,6 +27,52 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(res.ContentLength)
+
+	contentLength := int(res.ContentLength)
+
+	fmt.Println(contentLength)
+
+	bytesPerThread := contentLength / numThreads
+	fmt.Println(bytesPerThread)
+
+	startByte := 0
+	endByte := bytesPerThread
+
+	for i := 0; i < numThreads; i++ {
+
+		wg.Add(1)
+		go downloadPart(url, startByte, endByte, &wg)
+
+		startByte = endByte + 1
+		endByte = startByte + bytesPerThread
+
+		if endByte > contentLength {
+			endByte = contentLength
+		}
+	}
+
+	wg.Wait()
+
+}
+
+func downloadPart(url string, startByte int, endByte int, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	fmt.Println("start: ", startByte)
+	fmt.Println("end: ", endByte)
+
+	client := new(http.Client)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", startByte, endByte))
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(resp.ContentLength)
 
 }
